6c3657c8c57e925920238677cbc7a6bcedb446f8
VitorPeixoto <vitorpeixotoc137@gmail.com>
2023-07-27 00:37:58 -0300

    .env add

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import { StyleSheet, FlatList } from "react-native";
+import { useState, useEffect } from 'react'
+import { StyleSheet, FlatList, ActivityIndicator } from "react-native";
 import apodsJson from '../src/data/apods.json'
 import ApodListItem from "../src/components/ApodListItem";
 import FullScreenImage from '../src/components/FullScreenImage';
 import { Apod } from '../src/types';
+import { fetchApods } from '../src/api/apods';
 
 export default function Page() {
-  const [apods, setApods] = useState<Apod[]>(apodsJson)
+  const [apods, setApods] = useState<Apod[]>(null)
   const [activePicture, setActivePicture] = useState<string>(null)
+
+  useEffect(() => {
+    fetchApods().then(setApods)
+  }, [])
+
+  if(!apods) return <ActivityIndicator/> 
 
   return (
     <>
